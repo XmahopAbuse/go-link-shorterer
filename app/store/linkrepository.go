293036b53677e36fecea51621b13fa70e487ef61
xmahop <xmahopnyan@yandex.ru>
2021-12-22 15:32:03 +0300

    link repository

--- app/store/linkrepository.go
+++ app/store/linkrepository.go
@@ -6,17 +6,21 @@ type LinkRepository struct {
 	store *Store
 }
 
-func (r *LinkRepository) Create (l *model.Link) (*model.Link, error){
-	if err := r.store.db.QueryRow(
-		"INSERT into links(url, shortUrl) VALUES ($1, $2)",
-			l.Url, l.ShortUrl);
-	err != nil {
-		return nil,nil
+func (r *LinkRepository) Create(l *model.Link) (*model.Link, error) {
+
+	if err := r.store.db.QueryRow("INSERT INTO links (short_url, url) values ($1, $2) RETURNING url, short_url", l.ShortUrl, l.Url).Scan(&l.Url, &l.ShortUrl); err != nil {
+		return nil, err
 	}
 
-	return nil, nil
+	return l, nil
 }
 
 func (r *LinkRepository) FindExistUrl(url string) (*model.Link, error) {
-	return nil, nil
+	l := &model.Link{}
+
+	if err := r.store.db.QueryRow("SELECT * from links where url = $1", url).Scan(&l.Url, &l.ShortUrl); err != nil {
+		return nil, err
+	}
+
+	return l, nil
 }
