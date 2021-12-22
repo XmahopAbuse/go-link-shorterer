40ad893c96e94df8a756bac605d3551d2353446e
xmahop <xmahopnyan@yandex.ru>
2021-12-22 15:31:29 +0300

    generate short url function

--- app/model/link.go
+++ app/model/link.go
@@ -1,6 +1,15 @@
 package model
 
+import "go-link-shorterer/app/utils"
+
 type Link struct {
-	Url	string
+	Url      string
 	ShortUrl string
+}
+
+func (l *Link) BeforeCreate() error {
+	if len(l.Url) > 0 {
+		l.ShortUrl = utils.GenerateShortUrl()
+	}
+	return nil
 }
--- app/utils/generateshorturl.go
+++ app/utils/generateshorturl.go
@@ -1,4 +1,4 @@
-package main
+package utils
 
 import (
 	"math/rand"
@@ -9,10 +9,10 @@ import (
 var symbols = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ123456789_")
 var shortLinkLength int = 10
 
-func generateShortUrl() string {
+func GenerateShortUrl() string {
 	rand.Seed(time.Now().UnixNano())
 	var shortUrl strings.Builder
-	for i := 1; i <= shortLinkLength ; i++ {
+	for i := 1; i <= shortLinkLength; i++ {
 		shortUrl.WriteRune(symbols[rand.Intn(len(symbols))])
 	}
 
