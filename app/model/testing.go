910a16631b25c1c03e17c9c335660bb6c2c28541
xmahop <xmahopnyan@yandex.ru>
2021-12-22 15:32:29 +0300

    database testing

---
+++ app/model/testing.go
@@ -0,0 +1,8 @@
+package model
+
+import "testing"
+
+// Нужен. чтобы каждый раз возвращаться пользователя с валидным набором данных
+func TestLink(t *testing.T) *Link {
+	return nil
+}
--- app/store/linkrepository_test.go
+++ app/store/linkrepository_test.go
@@ -4,17 +4,26 @@ import (
 	"github.com/stretchr/testify/assert"
 	"go-link-shorterer/app/model"
 	"go-link-shorterer/app/store"
+	"go-link-shorterer/app/utils"
 	"testing"
 )
 
 func TestLinkRepository_Create(t *testing.T) {
-	s, teardown := store.TestStore(t , databaseUrl)
-	defer teardown("links")
-
+	s, _ := store.TestStore(t, databaseUrl)
 	l, err := s.Link().Create(&model.Link{
-		Url: "https://google.com",
+		Url:      "example.com",
+		ShortUrl: utils.GenerateShortUrl(),
 	})
+	assert.NoError(t, err)
+	assert.NotNil(t, l)
+}
 
+func TestLinkRepository_FindExistUrl(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseUrl)
+	defer teardown("links")
+	url := "example.com"
+
+	l, err := s.Link().FindExistUrl(url)
 	assert.NoError(t, err)
 	assert.NotNil(t, l)
 }
