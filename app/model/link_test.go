c53a92e6a3fea2be6c3d612bda00cce012905ce1
xmahop <xmahopnyan@yandex.ru>
2021-12-23 14:54:57 +0300

    test link and refactor testing methods

--- app/model/link_test.go
+++ app/model/link_test.go
@@ -1,7 +1,6 @@
 package model_test
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"go-link-shorterer/app/model"
 	"testing"
@@ -9,6 +8,51 @@ import (
 
 func TestLink_GenerateShortUrl(t *testing.T) {
 	l := model.TestLink(t)
+	l.GenerateShortUrl()
 	assert.NotEmpty(t, l.ShortUrl)
-	fmt.Println(l)
+}
+
+func TestLink_Validate(t *testing.T) {
+	testCases := []struct {
+		name    string
+		l       *model.Link
+		isValid bool
+	}{
+		{
+			name:    "valid",
+			l:       model.TestLink(t),
+			isValid: true,
+		},
+		{
+			name: "empty url",
+			l: &model.Link{
+				Url: "",
+			},
+			isValid: false,
+		},
+		{
+			name: "invalid url",
+			l: &model.Link{
+				Url: "asdasdas",
+			},
+			isValid: false,
+		},
+		{
+			name: "https url",
+			l: &model.Link{
+				Url: "https://google.com",
+			},
+			isValid: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.l.Validate())
+			} else {
+				assert.Error(t, tc.l.Validate())
+			}
+		})
+	}
 }
